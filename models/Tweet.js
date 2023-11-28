056cb165ee0523d1728aa653d2ca8613c296bf1d
ThomiHughes <thomih44@gmail.com>
2023-11-28 12:32:03 -0300

    Update Tweet.js

--- models/Tweet.js
+++ models/Tweet.js
@@ -2,31 +2,32 @@ const { Model, DataTypes } = require("sequelize");
 
 class Tweet extends Model {
   static initModel(sequelize) {
-    Tweet.init({
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+    Tweet.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        content: {
+          type: DataTypes.TEXT(),
+          allowNull: false,
+        },
+        // dateCreated: {
+        //   type: DataTypes.DATE,
+        //   defaultValue: DataTypes.NOW,
+        // },
+        // authorId: {
+        //   type: DataTypes.INTEGER,
+        //   // references: {
+        //   //   model: 'User',
+        //   //   key: 'id',
+        //   // },
+        likes: {
+          type: DataTypes.INTEGER,
+          defaultValue: 0,
+        },
       },
-      content: {
-        type: DataTypes.TEXT(),
-        allowNull: false,
-      },
-      // dateCreated: {
-      //   type: DataTypes.DATE,
-      //   defaultValue: DataTypes.NOW,
-      // },
-      // authorId: {
-      //   type: DataTypes.INTEGER,
-      //   // references: {
-      //   //   model: 'User',
-      //   //   key: 'id',
-      //   // },
-      likes: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-      },
-    },
       {
         sequelize,
         modelName: "tweet", // Nombre del modelo en singular y en minúscula.
