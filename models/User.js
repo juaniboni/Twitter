18cab7995efd2260d5d8c17dddc15683b1840956
ThomiHughes <thomih44@gmail.com>
2023-11-28 12:31:19 -0300

    update

--- models/User.js
+++ models/User.js
@@ -2,44 +2,45 @@ const { Model, DataTypes } = require("sequelize");
 
 class User extends Model {
   static initModel(sequelize) {
-    User.init({
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      firstname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lastname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true,
+    User.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
         },
+        firstname: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        lastname: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true,
+          },
+        },
+        bio: {
+          type: DataTypes.TEXT,
+        },
+        profilePic: {
+          type: DataTypes.STRING, // URL
+        },
+
+        //  tweets: {
+        //    type: DataTypes.ARRAY(DataTypes.TEXT), // If storing tweet content directly in the user model
+        //  }
       },
-      bio: {
-        type: DataTypes.TEXT,
-      },
-      profilePic: {
-        type: DataTypes.STRING, // URL
-      },
-      
-      //  tweets: {
-      //    type: DataTypes.ARRAY(DataTypes.TEXT), // If storing tweet content directly in the user model
-      //  }
-    },
       {
         sequelize,
         modelName: "user", // Nombre del modelo en singular y en minúscula.
