c3d2239e510fb0cb66c202af030e52bade163ef7
juaniboni <juanignacioboni@gmail.com>
2023-12-06 18:31:40 -0300

    Cambios

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -2,19 +2,22 @@ const express = require("express");
 const router = express.Router();
 const tweetController = require("../controllers/TweetController");
 
-// Rutas relacionadas a los artículos:
-// ...
+  
+// Rutas relacionadas a los tweets:
 
+// Obtener todos los tweets
 router.get("/", tweetController.index);
 
-// esta para subir un tweet
-router.post("/", tweetController.store);
+// Crear un nuevo tweet
+router.post("/",  tweetController.store);
 
+// Obtener un tweet por ID
 router.get("/:id", tweetController.show);
 
-// esta ruta para updatear los likes del tweet
+// Actualizar likes del tweet
 router.patch("/:id/like", tweetController.update);
 
+// Eliminar un tweet
 router.delete("/:id", tweetController.destroy);
 
 module.exports = router;
